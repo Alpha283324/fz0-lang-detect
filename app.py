c820efb9c1613b7839ea3326daa1a3a2c86313d0
Alpha283324 <73769952+Alpha283324@users.noreply.github.com>
2025-10-06 20:39:32 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/health", methods=["GET"])
+@app.route("/health")
 def health():
-    return jsonify({"status": "running!"})
+    return jsonify({"status": "running"})
 
 if __name__ == "__main__":
-    # Render sets PORT in environment, default to 5000 locally
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
